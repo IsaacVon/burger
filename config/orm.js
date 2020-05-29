13a5937ca7d047070e71913d9b05d0c7f4f99bd3
Isaac Householder <isaachouseholder@gmail.com>
2020-05-29 15:49:57 -0700

    created orm

---
+++ config/orm.js
@@ -0,0 +1,13 @@
+// import MySQL connection
+var connection = require('./connection')
+
+function printQuestionMarks(num) {
+    var arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+  }
+
